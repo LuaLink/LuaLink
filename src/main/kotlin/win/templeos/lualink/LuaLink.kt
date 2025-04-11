65197c7513f0a76864d273bccde675ded547de4a
Saturn745 <gitcommits.8lvon@passinbox.com>
2025-04-11 06:21:17 -0700

    Remove some unneeded code

--- src/main/kotlin/win/templeos/lualink/LuaLink.kt
+++ src/main/kotlin/win/templeos/lualink/LuaLink.kt
@@ -7,24 +7,7 @@ class LuaLink : JavaPlugin() {
     private val luaManager = LuaManager(this)
 
     override fun onEnable() {
-        val scriptsDir = dataFolder.resolve("scripts")
-        if (!scriptsDir.exists()) {
-            scriptsDir.mkdirs()
-        }
-
         luaManager.loadAllScripts()
-        //this.componentLogger.info(MiniMessage.miniMessage().deserialize("<yellow>Attempting to load </yellow><green>${scriptsDir.listFiles()?.size ?: 0}</green><yellow> scripts...</yellow>"))
-
-        // Go through each folder in the scripts directory and try to load main.lua from it
-/*        scriptsDir.listFiles()?.forEach { folder ->
-            if (folder.isDirectory) {
-                val mainLua = folder.resolve("main.lua")
-                if (mainLua.exists()) {
-                    val scriptContent = mainLua.readText()
-                    luaManager.createScriptFromString(scriptContent, folder.name)
-                }
-            }
-        }*/
     }
 
     override fun onDisable() {

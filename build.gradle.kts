40b13b38e60377b21136dff421b44d43bd2442f7
Saturn745 <gitcommits.8lvon@passinbox.com>
2025-04-12 21:30:28 -0700

    Automatic building and publishing
    Automatically builds and publishes to Modrinth and Hangar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,31 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
 plugins {
     kotlin("jvm") version "2.0.21"
     id("de.eldoria.plugin-yml.paper") version "0.7.1"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("com.gradleup.shadow") version "8.3.6"
+    id("com.modrinth.minotaur") version "2.8.7"
+    id("io.papermc.hangar-publish-plugin") version "0.1.3"
 }
 
+val buildNum = System.getenv("GITHUB_RUN_NUMBER") ?: "SNAPSHOT"
+
 group = "win.templeos.lualink"
-version = "1.0-SNAPSHOT"
+version = "1.21.4-$buildNum"
 
 repositories {
     mavenLocal()
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven {
+        url = uri("https://maven.pkg.github.com/lualink/luajava")
+        name = "LuaJava"
+        credentials {
+            username = findProperty("gpr.actor") as String? ?: System.getenv("GITHUB_ACTOR")
+            password = findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
 }
 
 val luaJavaVersion:String by project
@@ -22,7 +36,7 @@ dependencies {
     // Paper API
     compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
     // LuaJava (cannot be easily relocated or downloaded at runtime)
-    implementation("party.iroiro.luajava:luajava:4.0.3-SNAPSHOT")
+    implementation("party.iroiro.luajava:luajava:4.0.3-SNAPSHOT") // Use our fork of the LuaJava library
     implementation("party.iroiro.luajava:luajit:$luaJavaVersion")
     // LuaJava natives (cannot be easily relocated or downloaded at runtime)
     runtimeOnly("party.iroiro.luajava:luajit-platform:$luaJavaVersion:natives-desktop")
@@ -37,6 +51,35 @@ paper {
     // This allows the plugin to access external plugins' APIs without depending on them.
     // This may cause conflicts if different versions of the same dependency are loaded by different plugins.
     hasOpenClassloader = true
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("lualink")
+    versionNumber.set(version.toString())
+    versionType.set("release")
+    uploadFile.set(tasks.shadowJar.get())
+    gameVersions.addAll("1.21.4")
+    loaders.addAll("paper", "purpur")
+    changelog.set(System.getenv("GIT_COMMIT_MESSAGE"))
+}
+
+hangarPublish {
+    publications.register("plugin") {
+        version.set(project.version as String)
+        id.set("LuaLink")
+        channel.set("Release")
+        changelog.set(System.getenv("GIT_COMMIT_MESSAGE"))
+
+        apiKey.set(System.getenv("HANGAR_API_KEY"))
+
+        platforms {
+            register(Platforms.PAPER) {
+                jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+                platformVersions.set(listOf("1.20.1", "1.20.2", "1.20.3", "1.20.4", "1.21.4"))
+            }
+        }
+    }
 }
 
 tasks {

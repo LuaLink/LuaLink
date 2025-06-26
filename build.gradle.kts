34c79c3a5fcdec3839e94850821eeece0e983295
Saturn745 <gitcommits.8lvon@passinbox.com>
2025-06-26 15:23:58 -0700

    Bump LuaJava (fixes #16)
    See https://github.com/LuaLink/luajava/commit/91992006372d49d7b472a9f2870fe87fa08a891e

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,7 +92,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.6")
+        minecraftVersion("1.21.5")
 
         // Make sure runServer is ran with -add-opens=java.base/java.util=ALL-UNNAMED
         jvmArgs(
